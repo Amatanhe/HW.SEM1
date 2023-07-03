bdbee32364c8774d60aee904683a273b8e175a9f
Tatiana Tcyganova <tatiana.kv@gmail.com>
2023-07-03 18:27:24 +0300

    New excersise 8

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿//Задача 2
+
+// Console.WriteLine ("Напишите первое число: ") ;
+// int a = Convert.ToInt32 (Console.ReadLine ()) ;
+// Console.WriteLine ("Напишите второе число: ") ;
+// int b = Convert.ToInt32 (Console.ReadLine ()) ;
+// int max = a;
+// if (a > max ) max = a;
+// if (b > max ) max = b;
+// Console.Write($" Max = {max}");
+
+
+//Задача 4
+
+// Console.WriteLine ("Напишите первое число: ") ;
+// int a = Convert.ToInt32 (Console.ReadLine ()) ;
+// Console.WriteLine ("Напишите второе число: ") ;
+// int b = Convert.ToInt32 (Console.ReadLine ()) ;
+// Console.WriteLine ("Напишите третье число: ") ;
+// int c = Convert.ToInt32 (Console.ReadLine ()) ;
+// int max = a;
+// if (a > max ) max = a;
+// if (b > max ) max = b;
+// if (c > max ) max = c;
+// Console.Write($" Max = {max}");
+
+
+//Задача 6
+// Console.WriteLine ("Напишите число: ") ;
+// int number = Convert.ToInt32 (Console.ReadLine ()) ;
+// if (number < 0)
+// {
+// number=number * (-1);
+// }
+// if (number % 2 == 0)
+// {
+// Console.WriteLine ("Четное") ;
+// }
+// else
+// {
+// Console.WriteLine ("Нечётное ") ;
+// };
+
+
+//Задача 8
+
+
+// Console.WriteLine ("Напишите число: ") ;
+// int finish = Convert.ToInt32 (Console.ReadLine ()) ;
+// int start = 0;
+// if (start < finish);
+// while (start <= finish)
+// {
+// Console.Write (start+ ",");
+// start = start + 2;
+// }
